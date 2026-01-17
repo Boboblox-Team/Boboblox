5f2a831051cd1701324fb9bdc9a9d3cabf846eab
Bobo <abdulsamad.arilewola@gmail.com>
2026-01-17 17:46:14 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/Navbar";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
