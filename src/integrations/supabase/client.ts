e7c3ae73917a000429d45fd04263f5f0b8e1facf
ThatBobo <abdulsamad.arilewola@gmail.com>
2026-03-08 14:17:51 +0000

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -5,7 +5,7 @@ import type { Database } from './types';
 const SUPABASE_URL = "https://girurweqftroscythxje.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdpcnVyd2VxZnRyb3NjeXRoeGplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEwNzg5MjIsImV4cCI6MjA4NjY1NDkyMn0.NMsyx9RHaSTCtPvvpzwr-1ClZHO0l81OClgQNEJ6p2Q";
 
-export const supabase = createClient<Database>(SUPABASE_URL, ANON_KEY, {
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
     storage: localStorage,
     persistSession: true,
